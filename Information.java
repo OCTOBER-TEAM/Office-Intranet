e3cf9a8e9c9101bc7488dba3a0e900606fb0ecf5
Mbuyiselo Ntozinkulu <mntozinkulu@cognadev.com>
2017-01-22 14:11:34 +0200

    Second Commit

--- Information.java
+++ Information.java
@@ -1,6 +1,12 @@
+Import java.util.*;
+ 
 Public class Information {
 
 public static void main(String [] args) {
  System.ou.printLine("I have started");
+   }
+   
+   public logFile(String msg) {
+	   System.out.printLine(LocalDate.now()+" -- "+msg+" --\n\r")
    }
 }
